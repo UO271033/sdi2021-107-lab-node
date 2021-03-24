3916c6ef92e992c0336a8f0e25306c072a93755b
xurde <uo271033@uniovi.es>
2021-03-24 10:35:17 +0100

    SDI-2021-107-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -27,5 +27,15 @@ module.exports = function(app) {
         res.send(respuesta);
     });
 
+    app.post("/cancion", function(req, res) {
+        res.send("Canción agregada: " + req.body.nombre + "<br>"
+         + " genero: " + req.body.genero + "<br>"
+         + " precio: " + req.body.precio);
+    });
+
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta patrón promo* ');
+    })
+
 
 };
