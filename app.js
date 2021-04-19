afbfa481441238f47f7e908aa89c28c529c09f60
xurde <uo271033@uniovi.es>
2021-04-19 21:05:00 +0200

    SDI-2021-107-10.1-SW API REST CRUD Canciones.

--- app.js
+++ app.js
@@ -107,6 +107,8 @@ app.set('crypto',crypto);
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rapicanciones.js")(app, gestorBD);
+
 
 app.get('/', function (req, res) {
     res.redirect('/tienda');
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -330,6 +330,6 @@ module.exports = function(app, swig, gestorBD) {
             }
 
         });
-    }
+    };
 
 };
